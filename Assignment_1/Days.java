0c351737561b7497c74fc50741ceea3435f45336
Swapnali Patil <152301536+SwapnaliPatil5@users.noreply.github.com>
2024-03-18 14:59:53 +0530

    Add files via upload

---
+++ Assignment_1/Days.java
@@ -0,0 +1,44 @@
+package Assignment_1;
+
+import java.util.Scanner;
+public class Days
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int daynum = 0;
+ 
+		System.out.printf("Enter the Weekday Number : ");
+		daynum = input.nextInt();
+ 
+		switch (daynum)
+		{
+		case 0:
+			System.out.printf("Sunday");
+			break;
+		case 1:
+			System.out.printf("Monday");
+			break;
+		case 2:
+			System.out.printf("Tuesday");
+			break;
+		case 3:
+			System.out.printf("Wednesday");
+			break;
+		case 4:
+			System.out.printf("Thursday");
+			break;
+		case 5:
+			System.out.printf("Friday");
+			break;
+		case 6:
+			System.out.printf("Saturday");
+			break;
+		default:
+			System.out.printf("Invalid...Please Enter the 0 to 6...");
+			break;
+
+		}
+		input.close();
+	}
+}
